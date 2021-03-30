e4f3ff73878eed33365e57d304bcdb0daa5e252e
Mayukh Basu <roxy.firebird@gmail.com>
2021-03-30 20:48:02 +0530

    added password hashing

---
+++ auth/src/models/user.ts
@@ -0,0 +1,42 @@
+import mongoose, {Schema} from 'mongoose';
+import {  Password} from "../services/password";
+
+interface UserAttr {
+    email: string;
+    password: string;
+
+}
+interface UserDoc extends mongoose.Document {
+    email: string;
+    password: string;
+}
+interface UserModel extends mongoose.Model<any> {
+    build(attrs: UserAttr): UserDoc;
+}
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+userSchema.statics.build = (attr: UserAttr) => {
+    return new User(attr);
+}
+
+userSchema.pre('save', async function(done){
+    if(this.isModified('password')) {
+        const hashed = await Password.toHash(this.get('password'));
+        this.set('password', hashed);
+    }
+    done();
+})
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export {User};
