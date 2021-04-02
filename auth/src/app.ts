b29427eec88f42f671e8f308e3fb367e0473a08c
Mayukh Basu <roxy.firebird@gmail.com>
2021-04-02 15:27:36 +0530

    added first jest test case

---
+++ auth/src/app.ts
@@ -0,0 +1,27 @@
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+import cookieSession = require("cookie-session");
+import { json } from "body-parser";
+import express = require("express");
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: true
+}))
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', async(req, res) => {
+    throw new NotFoundError();
+})
+app.use(errorHandler);
+export {app};
