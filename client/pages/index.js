4d4aeddd984c4a049b24acb9406bb64d9d9e2208
Mayukh Basu <roxy.firebird@gmail.com>
2021-05-18 12:40:17 +0530

    finsihed UI

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,27 +1,19 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
-const LandingPage =  ({currentUser}) => {
-  console.log(currentUser);
-  
-  
-  return <h1>Landing Page</h1>;
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
 };
-LandingPage.getInitialProps = async({req}) => {
-  console.log(req.headers);
-  if(typeof window === 'undefined') {
-    
-    const {data}= await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-      headers: {
-        Host: 'ticketing.dev',
-        
-      }
-    });
-    return data;
-  } else {
-    const {data} = await axios.get('/api/users/currentuser');
 
-    return data;
-  }
-  
-}
+LandingPage.getInitialProps = async context => {
+  console.log('LANDING PAGE!');
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
+};
+
 export default LandingPage;
